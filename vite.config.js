0a4544cb2d31ab5986e2c98b8fc0196869ffeb1d
Himanshu B A <88345357+Himanshu-B-A@users.noreply.github.com>
2026-02-05 22:05:56 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Profile_Website_Himanshu-B-A/',
+  base: '/test-pol/',
   build: {
     outDir: 'dist',
   },
